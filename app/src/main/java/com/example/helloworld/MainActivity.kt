13439187e830fce92e69f4a7a933c2a19800fbe7
liyy0 <yuyanli@bu.edu>
2024-01-25 00:35:40 -0500

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.helloworld
+
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.helloworld.ui.theme.HelloWorldTheme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContentView(R.layout.activity_main)
+
+            val clickme = findViewById<Button>(R.id.click_me)
+            val helloText = findViewById<TextView>(R.id.helloText)
+
+            clickme.setOnClickListener(object : View.OnClickListener {
+                override fun onClick(v: View?) {
+                    helloText.text = "What's your name"
+                }
+            })
+        }
+
+
+    }
+}
